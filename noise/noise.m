1d94ad33d7ef19acc2ac30bde2a731598d07bec0
nmailoa <nmailoa@berkeley.edu>
2016-04-12 18:16:50 -0700

    noise stuff

---
+++ noise/noise.m
@@ -0,0 +1,15 @@
+data = csvread('noisespec.csv');
+
+freqs = data(:,1);
+Ts = 1/250e6;
+resp = 2*(exp(j*freqs*2*pi*Ts)-1);
+
+input_referred = sqrt(data(:,2)).*abs(resp);
+
+semilogx(freqs, input_referred)
+xlabel('Frequency (Hz)')
+ylabel('Noise Density (V/sqrt(Hz))')
+title('Input-referred Noise Density of Integrator 1')
+grid on
+
+int = sqrt(trapz(freqs(2:end)-freqs(1:end-1), (input_referred(1:end-1)).^2))
