d89c3742fac4b245591e9c5f71a07af1f4057a29
nmailoa <nmailoa@berkeley.edu>
2016-05-08 12:19:09 -0700

    noise

--- noise/noise.m
+++ noise/noise.m
@@ -1,6 +1,6 @@
 % stage 1
 
-data = csvread('noisespec1.csv');
+data = csvread('proj-noise-realboost-400.csv');
 
 freqs = data(:,1);
 Ts = 1/250e6;
@@ -8,11 +8,11 @@ resp = 2*(exp(j*freqs*2*pi*Ts)-1);
 
 input_referred = sqrt(data(:,2)).*abs(resp);
 
-semilogx(freqs, input_referred)
-xlabel('Frequency (Hz)')
-ylabel('Noise Density (V/sqrt(Hz))')
-title('Input-referred Noise Density of Single Stage Integrator 1')
-grid on
+%semilogx(freqs, input_referred)
+%xlabel('Frequency (Hz)')
+%ylabel('Noise Density (V/sqrt(Hz))')
+%title('Input-referred Noise Density of Single Stage Integrator 1')
+%grid on
 
 int = sqrt(trapz(freqs(2:end)-freqs(1:end-1), (input_referred(1:end-1)).^2))
 
@@ -20,7 +20,7 @@ int = sqrt(trapz(freqs(2:end)-freqs(1:end-1), (input_referred(1:end-1)).^2))
 
 % stage 2
 
-data = csvread('noisespec2.csv');
+data = csvread('proj-noise-realboost-400.csv');
 
 freqs = data(:,1);
 Ts = 1/250e6;
@@ -28,10 +28,10 @@ resp = 4*(exp(j*freqs*2*pi*Ts)-1).^2;
 
 input_referred = sqrt(data(:,2)).*abs(resp);
 
-semilogx(freqs, input_referred)
-xlabel('Frequency (Hz)')
-ylabel('Noise Density (V/sqrt(Hz))')
-title('Input-referred Noise Density of Single Stage Integrator 2')
-grid on
+%semilogx(freqs, input_referred)
+%xlabel('Frequency (Hz)')
+%ylabel('Noise Density (V/sqrt(Hz))')
+%title('Input-referred Noise Density of Single Stage Integrator 2')
+%grid on
 
 int = sqrt(trapz(freqs(2:end)-freqs(1:end-1), (input_referred(1:end-1)).^2))
